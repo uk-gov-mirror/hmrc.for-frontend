4a402eaea1546e2b60399ebdd786367c0b6f1899
Radek Hubner <rhubner@users.noreply.github.com>
2019-05-14 17:07:02 +0100

    Add sumary page and minor updates on first.

--- app/controllers/NotConnectedController.scala
+++ app/controllers/NotConnectedController.scala
@@ -16,6 +16,8 @@
 
 package controllers
 
+import controllers.feedback.SurveyFeedback
+import models.Satisfaction
 import models.pages.Summary
 import models.serviceContracts.submissions.Address
 import org.joda.time.DateTime
@@ -24,35 +26,48 @@ import uk.gov.hmrc.play.frontend.controller.FrontendController
 import play.api.i18n.Messages.Implicits._
 import uk.gov.hmrc.http.{HeaderCarrier, SessionKeys, Upstream4xxResponse}
 import play.api.Play.current
+import play.api.data.{Form, Forms}
+import form.Formats._
+import play.api.data.Forms.{mapping, text}
 
 object NotConnectedController extends FrontendController {
 
+  val summary = Summary (
+    referenceNumber = "10643313719",
+    journeyStarted = DateTime.now(),
+    propertyAddress = None,
+    customerDetails = None,
+    theProperty = None,
+    sublet = None,
+    landlord = None,
+    lease = None,
+    rentReviews = None,
+    rentAgreement = None,
+    rent = None,
+    rentIncludes = None,
+    incentives = None,
+    responsibilities = None,
+    alterations = None,
+    otherFactors = None,
+    address = Option(Address("Unit 7", Option("Maltings Industrial Estate"), Option("Charmley"), "CH1 1AA"))
+  )
+
+
+  val completedFeedbackForm = Form(mapping(
+    "satisfaction" -> Forms.of[Satisfaction],
+    "details" -> text(maxLength = 1200)
+  )(SurveyFeedback.apply)(SurveyFeedback.unapply))
 
   def onPageView = Action { implicit request =>
 
-    val summary = Summary (
-      referenceNumber = "10643313719",
-      journeyStarted = DateTime.now(),
-      propertyAddress = None,
-      customerDetails = None,
-      theProperty = None,
-      sublet = None,
-      landlord = None,
-      lease = None,
-      rentReviews = None,
-      rentAgreement = None,
-      rent = None,
-      rentIncludes = None,
-      incentives = None,
-      responsibilities = None,
-      alterations = None,
-      otherFactors = None,
-      address = Option(Address("Unit 7", Option("Maltings Industrial Estate"), Option("Charmley"), "CH1 1AA"))
-    )
-
-
 
     Ok(views.html.notConnected(Some(summary)))
+
+  }
+
+  def onPageSubmitView() = Action { implicit request =>
+
+    Ok(views.html.notConnecteConfirm(completedFeedbackForm, summary.referenceNumber, summary))
 
   }
 
