5934d0cfd7e79b260cf55a3a2d0df2d15f906db2
Yuriy Tumakha <tumakha@gmail.com>
2022-04-12 10:26:58 +0100

    VOA-3305 RALD Migrate to hmrc-mongo and MongoCacheRepository. :warning: :bangbang: Before deployment to production the shuttering service is recommended to close all active sessions. (#547)
    * :warning: :bangbang: VOA-3305 Before deployment to production the shuttering service is recommended to close all active sessions.
    VOA-3305 RALD Migrate to hmrc-mongo

--- app/form/persistence/MongoSessionRepository.scala
+++ app/form/persistence/MongoSessionRepository.scala
@@ -16,113 +16,41 @@
 
 package form.persistence
 
-import java.time.Instant
+import play.api.Configuration
+import play.api.libs.json.{Reads, Writes}
+import uk.gov.hmrc.mongo.cache.CacheIdType.SimpleCacheId
+import uk.gov.hmrc.mongo.cache.{CacheItem, DataKey, MongoCacheRepository}
+import uk.gov.hmrc.mongo.{MongoComponent, TimestampSupport}
 
+import java.util.concurrent.TimeUnit.SECONDS
 import javax.inject.{Inject, Singleton}
-import play.api.Logger
-import play.api.libs.json.{JsObject, JsValue, Json, Reads, Writes, __}
-import play.modules.reactivemongo.ReactiveMongoComponent
-import reactivemongo.api.collections.bson.BSONCollection
-import reactivemongo.api.commands.UpdateWriteResult
-import reactivemongo.api.indexes.{Index, IndexType}
-import reactivemongo.bson.BSONDocument
-import reactivemongo.play.json.ImplicitBSONHandlers._
-
+import scala.concurrent.duration.Duration
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class MongoSessionRepository @Inject() (mongo: ReactiveMongoComponent, ec: ExecutionContext) {
+class MongoSessionRepository @Inject()(
+                                   config: Configuration,
+                                   mongo: MongoComponent,
+                                   timestampSupport: TimestampSupport
+                                 )(implicit ec: ExecutionContext)
+  extends MongoCacheRepository(
+    mongoComponent = mongo,
+    collectionName = "sessionFormData",
+    ttl = Duration(config.get[Long]("session.timeoutSeconds"), SECONDS),
+    timestampSupport = timestampSupport,
+    cacheIdType = SimpleCacheId
+  ) {
 
-  val collection: BSONCollection = mongo.mongoConnector.db().collection("sessionFormData")
+  def fetchAndGetEntry[T](cacheId: String, key: String)(implicit rds: Reads[T], ec: ExecutionContext): Future[Option[T]] =
+    get[T](cacheId)(DataKey(key))
 
-  val logger = Logger(getClass())
+  def cache[A](cacheId: String, formKey: String, body: A)(implicit wts: Writes[A], ec: ExecutionContext): Future[CacheItem] =
+    put(cacheId)(DataKey(formKey), body)
 
-  ensureIndexes() //TODO - Change to standard index definition
+  def removeCache(cacheId: String): Future[Unit] =
+    deleteEntity(cacheId)
 
-  def ensureIndexes(): Future[Unit] = {
-    implicit  val executionContext = ec
-
-    val index = Index(Seq("lastUpdated" -> IndexType.Ascending),
-      name = Some("lastUpdatedIndex"),options = BSONDocument("expireAfterSeconds" -> 3600)
-    )
-    collection.indexesManager.ensure(index).recover{
-      case ex: Exception => {
-        logger.error("Unable to create ttl index on session collection", ex)
-        false
-      }
-    }.map(_ => ())
-
-  }
-
-
-  def fetchAndGetEntry[T](cacheId: String, key: String)( implicit rds: Reads[T], executionContext: ExecutionContext): Future[Option[T]] = {
-
-    val q = Json.obj("_id" -> cacheId)
-    val projection = Json.obj(key -> 1)
-
-    collection.find(q, projection = Some(projection)).one[JsObject].map { maybeDocument =>
-      maybeDocument
-        .flatMap ( document => document.value.get(key))
-        .flatMap( x => rds.reads(x).asOpt)
-    }
-
-  }
-
-  /*
-     {
-       _id: "88993-234234-234234-sessionId-cacheId",
-       ttl: BSONDate(2019-09-24T10:09:01+0000),
-       9999000123: { // formId
-         page1: {
-           inputFieldName: "value",
-           landlordName: "landlordName"
-         },
-         page2: {
-           asdasasda: "asdad"
-         }
-       },
-       9999000321: {
-         page1: {
-           inputFieldName: "value",
-           landlordName: "landlordName"
-         },
-         page2: {
-           asdasasda: "asdad"
-         }
-       }
-      */
-
-
-  def cache[A](cacheId: String, formId: String, body: A)(implicit wts: Writes[A], executionContext: ExecutionContext): Future[UpdateWriteResult] = {
-    val q = Json.obj("_id" -> cacheId)
-
-    val u = Json.obj(
-      "$set" -> Json.obj(
-        "lastUpdated" -> Instant.now(),
-        formId -> wts.writes(body)
-      )
-    )
-    collection.update(false).one(q,u, upsert = true)
-  }
-
-  def removeCache(cacheId: String) = {
-    val q = Json.obj("_id" -> cacheId)
-    implicit val _ec = ec
-    collection.delete(false).one(q, limit = Option(1)).map(_ => ())
-  }
-
-
-  implicit val instantTimeRead: Reads[Instant] =
-    (__ \ "$date").read[Long].map { dateTime =>
-      Instant.ofEpochMilli(dateTime)
-    }
-
-  implicit val instantTimeWrite: Writes[Instant] = new Writes[Instant] {
-    override def writes(instant: Instant): JsValue = Json.obj(
-      "$date" -> ((instant.getEpochSecond * 1000L) + (instant.getNano / 1000000L))
-    )
-  }
-
-
+  def remove(cacheId: String, key: String): Future[Unit] =
+    delete(cacheId)(DataKey(key))
 
 }
