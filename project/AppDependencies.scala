395fb632382ce0934ee1da10da06b8107aa62afe
Yuriy Tumakha <tumakha@gmail.com>
2024-02-12 10:51:45 +0000

    VOA-3539 Remove encryption and leave only one-way hash (Deploy after 4 March 2024)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion    = "8.3.0"
-  val playFrontendVersion = "8.2.0"
-  val mongoVersion        = "1.6.0"
+  val bootstrapVersion    = "8.4.0"
+  val playFrontendVersion = "8.4.0"
+  val mongoVersion        = "1.7.0"
 
   // Test dependencies
-  val scalatestPlusPlayVersion = "7.0.0"
-  val scalatestVersion         = "3.2.17"
+  val scalatestPlusPlayVersion = "7.0.1"
+  val scalatestVersion         = "3.2.18"
   val mockitoScalaVersion      = "1.17.30"
   val flexMarkVersion          = "0.64.8"
 
@@ -19,7 +19,7 @@ object AppDependencies {
     "uk.gov.hmrc"               %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"               %% "play-frontend-hmrc-play-30" % playFrontendVersion,
     "uk.gov.hmrc.mongo"         %% "hmrc-mongo-play-30"         % mongoVersion,
-    "org.xhtmlrenderer"          % "flying-saucer-pdf-itext5"   % "9.1.22",
+    "org.xhtmlrenderer"          % "flying-saucer-pdf-itext5"   % "9.5.1",
     "nu.validator"               % "htmlparser"                 % "1.4.16",
     "org.webjars"                % "jquery"                     % "3.7.1",
     "com.github.java-json-tools" % "json-schema-validator"      % "2.2.14", // must be the same version as in "for-hod-adapter"
